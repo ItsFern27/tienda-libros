a320ee12929eb0007621f64c6be5e271f8dbea2f
ItsFern27 <fjarevalo07@gmail.com>
2025-11-02 19:08:42 -0500

    cards beta

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -2,14 +2,20 @@
 Aqui se manda el contenido al layout
 */
 import Link from 'next/link'
+import { BookCard } from '../components/card/book';
 
-export default function Home() {
+export default async function Home() {
+  
+
   return (
     <div className='flex flex-col items-start'> {/* borrar items-start para comportamiento normal de flex */}
       <h1>index main </h1>
       <Link href="/stores" className='bg-red-500'>link a stores</Link>
       <Link href="/search" className='bg-emerald-700'>link a search</Link>
       <Link href="/configuration" className='bg-blue-700'>link a conf</Link>
+
+      <p>Componente</p>
+      <BookCard/>
     </div>
   );
 }
---
+++ src/app/components/card/book.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+import { createClient } from "@/utils/supabase/server";
+
+export async function BookCard() {
+    const supabase = await createClient();
+    const { data: libros } = await supabase.from('libro').select('*').limit(5);
+
+    return (
+        <div>
+            <pre>{JSON.stringify(libros, null, 2)}</pre>
+
+            <div className="flex flex-row items-start gap-4">
+                {libros?.map((libro) => (
+                    <div
+                        key={libro.id}
+                        className="max-w-60 w-full flex flex-col cursor-pointer bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl hover:scale-102 transition-[transform, shadow] duration-300"
+                    >
+                        {/* Imagen del libro */}
+                        <img
+                            src={libro.portada} // <-- aquí tu campo de la URL de la portada
+                            alt={libro.titulo}
+                            className="h-90 object-contain"
+                        />
+
+                        {/* Contenido del libro */}
+                        <div className="flex flex-col justify-between p-4">
+                            <div>
+                                <p className="text-sm text-gray-600">{libro?.autor}</p>
+                                <h2 className="text-lg font-bold text-gray-800 h-12 leading-6 line-clamp-2">{libro?.titulo}</h2>
+                            </div>
+                            <p className="mt-2 font-semibold text-green-600">S/.{libro?.precio}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
